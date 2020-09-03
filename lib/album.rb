b2470cee843a80d284714ec5cfd0f352caaa56b7
Joshua Lovelace <rakmial@gmail.com>
2020-09-03 12:40:16 -0700

    add Sinatra CRUD / testing work up to 'before(:each)'

---
+++ lib/album.rb
@@ -0,0 +1,29 @@
+class Album
+  attr_reader :id, :name
+  
+  @@albums = {}
+  @@total_rows = 0
+
+  def initialize name, id
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@albums.values
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(other_album)
+    self.name == other_album.name
+  end
+
+end
---
+++ spec/album_spec.rb
@@ -0,0 +1,44 @@
+require('rspec')
+require('album')
+
+describe('#Album') do
+  before(:each) do
+    Album.clear
+  end
+  
+  describe('.all') do
+    it('is empty when no albums have been created') do
+      expect(Album.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears albums from hash') do
+      album = Album.new("Giant Steps", nil)
+      album.save
+      album2 = Album.new("Iowa", nil)
+      album2.save
+      Album.clear
+      expect(Album.all).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it('is the same if the attributes are the same') do
+      album = Album.new("Blue", nil)
+      album2 = Album.new("Blue", nil)
+      expect(album).to(eq(album2))
+    end
+  end
+
+  describe('#save') do
+    it('saves an album to albums hash') do
+      album = Album.new("Giant Steps", nil)
+      album.save()
+      album2 = Album.new("Blue", nil)
+      album2.save()
+      expect(Album.all).to(eq([album,album2]))
+    end
+  end
+
+end
